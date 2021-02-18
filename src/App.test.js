2833d231ac60196cbb10fc712fa8edc3f26857fc
heeyoung <heeyoung.jeong@naviworks.com>
2021-02-18 16:47:50 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders k', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders k', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
